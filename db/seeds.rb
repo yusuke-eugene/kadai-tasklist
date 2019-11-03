598d2b2da56b0c1c8e74d3057a7272446bf85da6
yusuke-eugene <wunm61558@gmail.com>
2019-11-03 10:30:26 +0000

    add db-seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+(1..100).each do |number|
+    Message.create(title: 'test status ' + number.to_s, content: 'test content ' + number.to_s)
+end    
